8ddacf52b750b31f29d48d3d5f7043cf082f2c0d
madpirat <bzyczek@gmail.com>
2022-05-26 22:59:51 +0200

    JavaScript conditional statements update commit

--- All_Over/Sections/js/js-starter.js
+++ All_Over/Sections/js/js-starter.js
@@ -10,7 +10,7 @@ var schoolNumber = 20;
 var isRegisterd = true;
 console.log(isRegisterd);
 
-console.log(name + " " + schoolNumber) 
+console.log(name + " " + schoolNumber)
 
 var schoolName, hasCourse;
 schoolName = "OAKAcademy";
@@ -34,20 +34,46 @@ console.log("You are " + age + " years old!");
 
 // Basic Operators //
 
-var totalDistance, distanceAlex, distanceSam;
-totalDistance = 20;
+// var totalDistance, distanceAlex, distanceSam;
+// totalDistance = 20;
 
-distanceAlex = totalDistance - 12;
-console.log(distanceAlex);
+// distanceAlex = totalDistance - 12;
+// console.log(distanceAlex);
 
-distanceSam = totalDistance - 7;
-console.log(distanceSam);
+// distanceSam = totalDistance - 7;
+// console.log(distanceSam);
 
-// Logical Operators //
-var distanceToSchool, distanceToHome;
+// // Logical Operators //
+// var distanceToSchool, distanceToHome;
 
-distanceToHome = 14;
-distanceToSchool = 6;
+// distanceToHome = 14;
+// distanceToSchool = 6;
 
-var isCloser = distanceToHome < distanceToSchool;
-console.log(isCloser);
+// var isCloser = distanceToHome < distanceToSchool;
+// console.log(isCloser);
+
+// Conditional Statements 
+
+// var grade;
+// grade = 55; 
+
+// if (grade < 40) {
+//     console.log('you can not go to the next class.');
+// } else if (grade > 40 && grade < 45) {
+//     console.log('you can not go to the next class but you can enter the exam.');
+// } else {
+//     console.log('You can go to the next class!');
+// }
+
+var age;
+age = 24;
+
+if (age < 18) {
+    console.log('You can not enter the cafe!');
+} else if (age < 23) {
+    console.log('You can enter the cafe but you can not drink alcohol!');
+} else {
+    console.log('You can enter the cafe and drink alcohol!');
+}
+
+
