907c5e850a5aae6998d2124f813541ea4fc9719c
madpirat <bzyczek@gmail.com>
2022-05-26 14:43:09 +0200

    JavaScript 3rd update commit

--- All_Over/Sections/js/js-starter.js
+++ All_Over/Sections/js/js-starter.js
@@ -16,3 +16,16 @@ schoolName = "OAKAcademy";
 hasCourse = true;
 
 console.log(schoolName + " has 35 courses. Does it have any courses? " + hasCourse)
+
+schoolNumber = "twenty two";
+console.log(schoolNumber);
+
+schoolTeam = "tennis";
+console.log(schoolTeam);
+
+alert(schoolTeam);
+
+var age = prompt("How old are you?");
+console.log("You are " + age + " years old!");
+
+
