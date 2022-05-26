c6932ba082e9a7bcdd58061bf6713b13918cb115
madpirat <bzyczek@gmail.com>
2022-05-26 14:22:25 +0200

    JavaScript 2nd update commit

--- All_Over/Sections/js/js-starter.js
+++ All_Over/Sections/js/js-starter.js
@@ -8,3 +8,11 @@ var schoolNumber = 20;
 
 var isRegisterd = true;
 console.log(isRegisterd);
+
+console.log(name + " " + schoolNumber) 
+
+var schoolName, hasCourse;
+schoolName = "OAKAcademy";
+hasCourse = true;
+
+console.log(schoolName + " has 35 courses. Does it have any courses? " + hasCourse)
