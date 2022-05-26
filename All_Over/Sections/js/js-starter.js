0a0cd4ddcfa964f040959d8f98799ff6cbd11a4c
madpirat <bzyczek@gmail.com>
2022-05-26 14:08:56 +0200

    JavaScript update commit

--- All_Over/Sections/js/js-starter.js
+++ All_Over/Sections/js/js-starter.js
@@ -1,1 +1,10 @@
 console.log("Hello from separated file!")
+
+var name = "Marcin";
+console.log(name);
+
+var schoolTeam = "basketball";
+var schoolNumber = 20;
+
+var isRegisterd = true;
+console.log(isRegisterd);
