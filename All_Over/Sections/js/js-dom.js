9c3a0f207a3c071f93d967a10a6ec64f2402f70e
madpirat <bzyczek@gmail.com>
2022-06-04 16:03:55 +0200

    JavaScript DOM Select Method update commit

---
+++ All_Over/Sections/js/js-dom.js
@@ -0,0 +1,17 @@
+var selectWithId = document.getElementById('oakParagraph');
+console.log(selectWithId);
+
+var selectWithClass = document.getElementsByClassName('background');
+console.log(selectWithClass);
+
+var selectWithTag = document.getElementsByTagName('h1');
+console.log(selectWithTag);
+
+var selectWithQueryId = document.querySelector('#oakParagraph');
+console.log(selectWithQueryId);
+
+var selectWithQueryClass = document.querySelector('.background');
+console.log(selectWithQueryClass);
+
+var selectWithQueryAllClass = document.querySelectorAll('.background');
+console.log(selectWithQueryAllClass);
