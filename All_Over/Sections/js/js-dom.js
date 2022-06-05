7d59edd30afab757fe7b797294ee38cde6e57860
madpirat <bzyczek@gmail.com>
2022-06-05 17:57:47 +0200

    JavaScript Manipulation Methods - DOM practice update commit

--- All_Over/Sections/js/js-dom.js
+++ All_Over/Sections/js/js-dom.js
@@ -1,3 +1,5 @@
+// SELECTION METHODS - DOM
+
 var selectWithId = document.getElementById('oakParagraph');
 console.log(selectWithId);
 
@@ -15,3 +17,20 @@ console.log(selectWithQueryClass);
 
 var selectWithQueryAllClass = document.querySelectorAll('.background');
 console.log(selectWithQueryAllClass);
+
+// MANIPULATION METHODS - DOM
+
+var divToManipulate = document.querySelector('.background');
+
+// divToManipulate.style.height = '50px';
+// divToManipulate.style.width = '400px';
+// divToManipulate.style.backgroundColor = 'red';
+// divToManipulate.style.fontSize = '30px';
+// divToManipulate.style.border = '5px solid black';
+
+// divToManipulate.classList.add('addClass');
+// divToManipulate.classList.remove('addClass');
+
+divToManipulate.classList.toggle('addClass');
+divToManipulate.classList.toggle('addClass');
+divToManipulate.classList.toggle('addClass');
