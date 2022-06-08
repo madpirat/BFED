d1dd21146a484761f772ce7efab97f663b9bbafa
madpirat <bzyczek@gmail.com>
2022-06-08 10:31:54 +0200

    JavaScript - NODE JS Serving HTML update commit

--- All_Over/Sections/node-js-tutorial/app.js
+++ All_Over/Sections/node-js-tutorial/app.js
@@ -149,8 +149,8 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    const readStream = fs.createReadStream(__dirname + '/read-me.txt', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const readStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
 
     readStream.pipe(res);
 });
