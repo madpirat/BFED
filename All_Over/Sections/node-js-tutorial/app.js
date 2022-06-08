6c319439a22fa993e69dbcfea181bca46c27cba4
madpirat <bzyczek@gmail.com>
2022-06-08 11:16:54 +0200

    JavaScript - NODE JS Serving JSON Data - update commit

--- All_Over/Sections/node-js-tutorial/app.js
+++ All_Over/Sections/node-js-tutorial/app.js
@@ -149,10 +149,16 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    const readStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    // const readStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
 
-    readStream.pipe(res);
+    // readStream.pipe(res);
+    const person = {
+        name: 'alex',
+        email: 'alex@email.com',
+        job: 'designer'
+    };
+    res.end(JSON.stringify(person));
 });
 
 server.listen(3000, '127.0.0.1');
