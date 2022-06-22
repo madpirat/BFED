4c33666c72ae8e2a58c5a409858bced751707df6
madpirat <bzyczek@gmail.com>
2022-06-22 14:14:57 +0200

    Classic Tetris - Redo commit

--- Classic_Tetris/js/main.js
+++ Classic_Tetris/js/main.js
@@ -1,16 +1,24 @@
 const canvas = document.getElementById('board');
-const cxt = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 // Calculate size of canvas from constants.
-cxt.canvas.width = COLS * BLOCK_SIZE;
-cxt.canvas.height = ROWS * BLOCK_SIZE;
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // Scale blocks
-cxt.scale(BLOCK_SIZE, BLOCK_SIZE);
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
-let board = new Board();
+// let board = new Board();
+
 
 function play() {
-    board.reset();
-    console.table(board.grid);
+    let board = new Board(ctx);
+    let piece = new Piece(ctx);
+    piece.draw();
+    board.piece = piece;
+    // board.reset();
+    // let piece = new Piece(ctx);
+    // piece.draw();
+
+    // board.piece = piece;
 }
