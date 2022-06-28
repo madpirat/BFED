f4e1b98e185748aa3922b9f1542949139186700f
madpirat <bzyczek@gmail.com>
2022-06-28 14:56:22 +0200

    Classic Tetris - rotate commit update

--- Classic_Tetris/js/tetris.js
+++ Classic_Tetris/js/tetris.js
@@ -80,6 +80,39 @@ function playerMove(dir) {
     }
 }
 
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                    matrix[y][x],
+                    matrix[x][y],
+                ];
+        }
+    }
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
 
 let dropCounter = 0;
 let dropInterval = 1000;
@@ -113,6 +146,10 @@ document.addEventListener('keydown', event => {
         playerMove(1);
     } else if (event.key === 'ArrowDown') {
         playerDrop();
+    } else if (event.key === 'q') {
+        playerRotate(-1);
+    } else if (event.key === 'w') {
+        playerRotate(1);
     }
 });
 
