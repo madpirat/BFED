f9fab0896a2ebcae8365e831a8aa59c3d3963521
madpirat <bzyczek@gmail.com>
2022-08-28 21:06:45 +0200

    JS - FizzBuzz Challenge

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,12 +1,38 @@
 // Here are some snippets of the code strictly for coding practice purposes
 
-// [Arrays] Guest List
+// // [Arrays] Guest List
 
-var guestList = ["Marcin", "Ania", "Maciek", "Agata", "Krzysiek", "Beata"];
-var guestName = prompt("What is your name?");
+// var guestList = ["Marcin", "Ania", "Maciek", "Agata", "Krzysiek", "Beata"];
+// var guestName = prompt("What is your name?");
 
-if (guestList.includes(guestName)) {
-    alert("Welcome " + guestName);
-} else {
-    alert("Sorry " + guestName + ", maybe next time!")
+// if (guestList.includes(guestName)) {
+//     alert("Welcome " + guestName);
+// } else {
+//     alert("Sorry " + guestName + ", maybe next time!")
+// }
+
+// FizzBuzz Challenge
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("FizzBuzz");
+    }
+
+    else if (count % 3 === 0) {
+        output.push("Fizz");
+    }
+    else if (count % 5 === 0) {
+        output.push("Buzz");
+    }
+
+    else {
+        output.push(count);
+    }
+
+    count++;
+    console.log(output);
+
 }
