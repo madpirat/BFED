afab2660ac9247e5cc61b19cdd922cb635cdb612
madpirat <bzyczek@gmail.com>
2022-08-31 21:40:56 +0200

    JS - FibonacciGenerator

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,5 +1,29 @@
 // Here are some snippets of the code strictly for coding practice purposes
 
+// Fibonacci Generator 
+
+function fibonacciGenerator(n) {
+
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2) {
+        output = [0, 1];
+    }
+    else {
+        output = [0, 1];
+
+        for (var i = 2; i < n; i++) {
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    return output;
+}
+
+output = fibonacciGenerator(5)
+console.log(output)
+
 // // [Arrays] Guest List
 
 // var guestList = ["Marcin", "Ania", "Maciek", "Agata", "Krzysiek", "Beata"];
@@ -36,3 +60,4 @@
 //     console.log(output);
 
 // }
+
