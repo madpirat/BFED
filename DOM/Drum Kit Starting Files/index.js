3df5087aae012892e0f90ba8b8865ea9ef724ddc
madpirat <bzyczek@gmail.com>
2022-09-25 16:26:46 +0200

    DrumKit - eventListener

--- DOM/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/index.js
@@ -1,9 +1,70 @@
+//Detecting Button Press
+
 document.querySelectorAll("button").forEach(function (handleClick) {
     handleClick.addEventListener("click", function () {
-        this.style.color = "#fff";
+
+        const buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
     });
+})
+
+//Dedecting Keyboard Press
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
 });
 
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            const tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
 
-// var audio = new Audio("sounds/tom-1.mp3")
-// audio.play();
+        case "a":
+            const tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play();
+            break;
+
+        case "s":
+            const tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play();
+            break;
+
+        case "d":
+            const tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play();
+            break;
+
+        case "j":
+            const snare = new Audio("sounds/snare.mp3")
+            snare.play();
+            break;
+
+        case "k":
+            const crash = new Audio("sounds/crash.mp3")
+            crash.play();
+            break;
+
+        case "l":
+            const kick = new Audio("sounds/kick-bass.mp3")
+            kick.play();
+
+        default: console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    const activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
